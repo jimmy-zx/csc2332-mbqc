4f2049190c93170069273748f02379d361a68457
Kai Z <104712557+Kai-Z23@users.noreply.github.com>
2025-12-17 22:48:54 -0500

    add graphix->qiskit poc and update requirements

--- poc/mbqc-x.py
+++ poc/mbqc-x.py
@@ -1,14 +1,21 @@
 from graphix import Circuit
+from graphix_ibmq.runner import IBMQBackend
+from matplotlib import pyplot as plt
 
 circ = Circuit(1)
 
-circ.x(0)
+circ.h(0)
 
 # Transpile to MBQC pattern
-pattern = circ.transpile().pattern
+pattern = circ.transpile()
 pattern.minimize_space()
 pattern.standardize()
 
 print(pattern.get_angles())
 print(pattern.get_meas_plane())
-pattern.draw_graph(flow_from_pattern=False, show_measurement_planes=True)
+# pattern.draw_graph()
+
+backend = IBMQBackend(pattern)
+backend.to_qiskit()
+backend.circ.draw('mpl')
+plt.show()
